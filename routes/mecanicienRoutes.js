a2bc798093bb2e6d0edc5a5da1836f444c57cc90
Daniella-286 <119035286+Daniella-286@users.noreply.github.com>
2025-03-29 08:38:47 +0100

    Update mecanicienRoutes.js

--- routes/mecanicienRoutes.js
+++ routes/mecanicienRoutes.js
@@ -6,6 +6,6 @@ const verifyToken = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.get("/non-valides", verifyToken, checkManagerRole, getMecaniciensNonValidesController);
-router.put("/valider/:id", verifyToken, checkManagerRole, validateMecanicienController);
+router.put("/valider/:id", validateMecanicienController);
 
 module.exports = router;
